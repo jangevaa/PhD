504f165385cbd5fb013e25bc73aafbbee3a90cd8
Justin Angevaare <jangevaa@uoguelph.ca>
2014-09-10 13:07:10 -0400

    Further organization of event sourcing approach

--- ILM/Simulation/events.py
+++ ILM/Simulation/events.py
@@ -3,3 +3,12 @@
 # September 2014
 # Justin Angevaare
 #################################
+
+# Susceptible to Infected event
+
+# Infected to Recovered event
+
+# Movement event
+
+# Change of other susceptibility parameter event
+
