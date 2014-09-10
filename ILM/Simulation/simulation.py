22144751be18de1568989294e3953dfd7ae5bd2e
Justin Angevaare <jangevaa@uoguelph.ca>
2014-09-10 13:17:41 -0400

    Define initial scope of simulation.py

---
+++ ILM/Simulation/simulation.py
@@ -0,0 +1,11 @@
+# Main simulation file
+# Justin Angevaare
+# September 2014
+
+# Generate initial individuals (locations and susceptibility parameters if applicable)
+
+# Generate initial infection(s)
+
+# Loop population update functions as desired
+
+# Output histories in CSV file
