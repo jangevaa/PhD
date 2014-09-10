25bc3404c5cf31b2d2621379a3e512afc6fbd4b2
Justin Angevaare <jangevaa@uoguelph.ca>
2014-09-10 13:11:20 -0400

    Define initial scope of individuals.py

---
+++ ILM/Simulation/individuals.py
@@ -0,0 +1,5 @@
+# Define individuals and their attributes
+# September 2014
+# Justin Angevaare
+
+# Individual information contains disease state, location, and any other variables which may relate to probability of infection
