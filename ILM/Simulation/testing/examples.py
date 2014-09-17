6824edd22347f535baa7ca258183afa757cafe95
jangevaa <jangevaa@uoguelph.ca>
2014-09-17 11:30:00 -0400

    begin work on MCMC - prior functions specifically

--- ILM/Simulation/testing/examples.py
+++ ILM/Simulation/testing/examples.py
@@ -1,13 +1,32 @@
 """examples.py - file for easily testing functionality and creating examples as program is built"""
 
+"""Generate population and propagate infection..."""
 pop = unif_indvs(500, 0, 10, 0, 10, 'test_pop')
 test = si_model(pop, 1, 10, 1, 10)
 
 """CSV output testing..."""
-test.to_csv("/Users/justin/Dropbox/Projects/[in progress] PhD research/ILM/Simulation/examples/example_event_database.csv")
-pop.to_csv("/Users/justin/Dropbox/Projects/[in progress] PhD research/ILM/Simulation/examples/example_pop.csv")
+savedir="/Users/justin/Dropbox/Projects/[in progress] PhD research/ILM/Simulation/testing/"
+
+test.to_csv(savedir+"example_event_database.csv")
+pop.to_csv(savedir+"example_pop.csv")
 
 """Plot output testing..."""
 for time in range(0,10):
     plot_infection(pop, test, time)
-    savefig("/Users/justin/Dropbox/Projects/[in progress] PhD research/ILM/Simulation/examples/example_time", time, ".png")
+    savefig(savedir + "example_time" + str(time) + ".png")
+
+"""Inference testing..."""
+
+"""Define prior distributions for alpha and beta"""
+import scipy as sp
+
+def prior_alpha(x):
+    gamma_alpha=5
+    gamma_beta=1
+    return pdf_gamma(gamma_alpha, gamma_beta, x)
+    
+def prior_beta(x):
+    gamma_alpha=5
+    gamma_beta=1
+    return pdf_gamma(gamma_alpha, gamma_beta, x)
+    
