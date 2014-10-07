d758ea21ed9639a8efc9aef32a3ca5dbf40ffb25
jangevaa <jangevaa@uoguelph.ca>
2014-10-07 13:22:05 -0400

    SIR MCMC with timing...

--- ILM/Simulation/testing/examples.py
+++ ILM/Simulation/testing/examples.py
@@ -1,5 +1,7 @@
 """examples.py - file for easily testing functionality and creating examples as program is built"""
 
+import time
+
 """Generate population and propagate infection with SI model..."""
 pop = unif_indvs(100, 0, 10, 0, 10, 'test_pop')
 test1 = si_model(pop, 1, 10, 1, 10)
@@ -53,6 +55,8 @@ savefig(savedir + "example_mcmc_beta.png")
 plt.plot(mcmc.density)
 savefig(savedir + "example_mcmc_density.png")
 
+################
+
 """Test SIcR model simulation..."""
 pop = unif_indvs(100, 0, 10, 0, 10, 'test_pop')
 test2 = sicr_model(pop, 1, 30, 1, 10, 3)
@@ -60,11 +64,12 @@ test2 = sicr_model(pop, 1, 30, 1, 10, 3)
 for time in range(1,30):
     plot_sir(pop, test1, time)
     savefig(savedir + "SIcR_example_time" + str(time) + ".png")
-    
+ 
+################
+      
 """Test SIR model simulation..."""
 pop = unif_indvs(100, 0, 10, 0, 10, 'test_pop')
 test3 = sir_model(pop, 1, 30, 1, 10, 3)
-
 
 """SIR Plotting..."""
 for time in range(1,(np.max(test3.time)+1)):
@@ -93,11 +98,6 @@ init_beta=np.random.gamma(5, 1)
 
 init_gamma=np.random.gamma(5, 1)
 
-mcmc=sir_infer(pop, test3, prior_alpha, init_alpha, prior_beta, init_beta, init_gamma, prior_gamma, 10, [[1,0,0,],[0,4,0],[0,0,1]])
-
-
-### TESTING
-t=21
-nonrecovered=find_nonrecovered(test3, t)
-susceptible=find_susceptible(pop, test3, t)
-infection_probs=infect_prob(pop, 1, 5, nonrecovered, susceptible)
+start_time=time.time()
+mcmc=sir_infer(pop, test3, prior_alpha, init_alpha, prior_beta, init_beta, init_gamma, prior_gamma, 500, [[1,0,0,],[0,4,0],[0,0,1]])
+end_time = time.time() - start_time
