bc46e316390e585e3b3c1c1a12dcc8f4b3231dec
jangevaa <jangevaa@uoguelph.ca>
2014-10-07 14:52:00 -0400

    Epidemic curve...

--- ILM/Simulation/output.py
+++ ILM/Simulation/output.py
@@ -13,7 +13,7 @@ def plot_si(pop, event_db, time):
     """This will create a simple scatterplot of susceptible and infectious individuals at
     a given time
     """
-    i=find_infectious(pop, event_db, time)
+    i=find_infectious(event_db, time)
     s=find_susceptible(pop, event_db, time)
     status=pd.DataFrame({"status":np.append(np.repeat("i", i.shape[0]), np.repeat("s", s.shape[0])), 
                          "colour":np.append(np.repeat("k", i.shape[0]), np.repeat("b", s.shape[0])),
@@ -26,12 +26,20 @@ def plot_sir(pop, event_db, time):
     """This will create a simple scatterplot of susceptible, infectious, and recovered
     individuals at a given time
     """
-    i=find_nonrecovered(pop, event_db, time)
+    i=find_nonrecovered(event_db, time)
     s=find_susceptible(pop, event_db, time)
-    r=find_recovered(pop, event_db, time)
+    r=find_recovered(event_db, time)
     status=pd.DataFrame({"status":np.append(np.append(np.repeat("i", i.shape[0]), np.repeat("s", s.shape[0])),np.repeat("r", r.shape[0])),
                          "colour":np.append(np.append(np.repeat("#a6cee3", i.shape[0]), np.repeat("#1f78b4", s.shape[0])),np.repeat("#b2df8a", r.shape[0])), 
                          "ind_ID":np.append(np.append(i.ind_ID, s.ind_ID), r.ind_ID),
                          "x":pop.iloc[np.append(np.append(i.ind_ID, s.ind_ID), r.ind_ID)].x,
                          "y":pop.iloc[np.append(np.append(i.ind_ID, s.ind_ID), r.ind_ID)].y})
     plt.scatter(status.x, status.y, c=status.colour, s=60, edgecolors='none')
+    
+def epi_curve(event_db):
+    """Display curve with the number of infected individuals at each time point"""
+    num_infected = [0]*(np.max(event_db.time)-1)
+    for t in range(1, np.max(event_db.time)):
+        num_infected[t-1] = find_nonrecovered(event_db, t).shape[0]
+    plt.scatter(range(1, np.max(event_db.time)), num_infected)
+                
