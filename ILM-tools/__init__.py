64a444df08254e75a74c5b111acadc550655acd5
jangevaa <jangevaa@uoguelph.ca>
2014-10-08 20:20:39 -0400

    reorganizing...

---
+++ ILM-tools/__init__.py
@@ -0,0 +1,6 @@
+from individuals import unif_indvs, norm_indvs, multi_groups
+from simulation import si_model, sicr_model, sir_model
+from inference import si_infer, sir_infer, pdf_gamma, pdf_unif, pdf_norm
+from output import plot_si, plot_sir, epi_curve
+
+__all__ = ['unif_indvs', 'norm_indvs', 'multi_groups', 'si_model', 'sicr_model', 'sir_model', 'si_infer', 'sir_infer', 'pdf_gamma', 'pdf_unif', 'pdf_norm', 'plot_si', 'plot_sir', 'epi_curve']
---
+++ ILM-tools/examples/si_example.py
@@ -0,0 +1,2 @@
+"""SI_example.py - file for easily testing functionality and creating examples as program is built"""
+
